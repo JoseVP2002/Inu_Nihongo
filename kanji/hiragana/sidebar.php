250af4f6e99619cd64f751e7c4a1c20cd70c6c2c
Dag Dvd <proyecto351daw@gmail.com>
2023-11-18 16:33:14 -0800

    Primer commit

---
+++ kanji/hiragana/sidebar.php
@@ -0,0 +1,9 @@
+<div class="container-fluid text-center">    
+  <div class="row content">
+    <div class="col-sm-2 sidenav">
+      <p align=left ><a href="introduccionhiragana.php">Introducción Hiragana</a></p>
+      <p align=left ><a href="crucigramasaludos.php">Ejercicio de practica 1</a></p>
+      <p align=left ><a href="sopadeletrasverbos.php">Ejercicio de practica 2</a></p>
+      <p align=left ><a href="memoramaadjetivos.php">Ejercicio de practica 3</a></p>
+    </div>
+    <div class="col-sm-8 text-left"> 
---
+++ kanji/kanji/numeros.php
@@ -0,0 +1,35 @@
+<?php
+include "../connection.php";
+include "Header.php";
+include "sidebar.php";
+?>
+
+      <center><h1><font color="green"><b>Números</b></font></h1></center>
+      <div class="col-sm-15-text-left">
+        <p class="justificado">1 - <ruby>一<rt>いち</rt></ruby> - Uno</p>
+        <p class="justificado">2 - <ruby>二<rt>に</rt></ruby> - Dos</p>
+        <p class="justificado">3 - <ruby>三<rt>さん</rt></ruby> - Tres</p>
+        <p class="justificado">4 - <ruby>四<rt>し/よん</rt></ruby> - Cuatro</p>
+        <p class="justificado">Nota: <font color="red">し</font> y <font color="red">よん</font> son pronunciaciones diferentes para el número 4. <font color="red">し</font> se utiliza comúnmente en contextos formales, mientras que <font color="red">よん</font> se utiliza en situaciones informales para evitar la asociación de sonido con la palabra para la muerte <ruby><font color="red">死</font><rt>し</rt></ruby>.</p>
+        <p class="justificado">5 - <ruby>五<rt>ご</rt></ruby> - Cinco</p>
+        <p class="justificado">6 - <ruby>六<rt>ろく</rt></ruby> - Seis</p>
+        <p class="justificado">7 - <ruby>七<rt>しち/なな</rt></ruby> - Siete</p>
+        <p class="justificado">Nota: <font color="red">しち</font> y <font color="red">なな</font> son pronunciaciones diferentes para el número 7. <font color="red">しち</font> se usa en contextos formales, mientras que <font color="red">なな</font> es más común en situaciones informales.</p>
+        <p class="justificado">8 - <ruby>八<rt>はち</rt></ruby> - Ocho</p>
+        <p class="justificado">9 - <ruby>九<rt>きゅう</rt></ruby> - Nueve</p>
+        <p class="justificado">10 - <ruby>十<rt>じゅう</rt></ruby> - Diez</p>
+        <p class="justificado">Estos kanjis son esenciales para aprender los números en japonés y son la base para construir números más grandes en el sistema numérico japonés.</p>
+        <center>
+          <a class="btn btn-danger" href="memoramanumeros.php" role="button">Practicar lectura de números</a>
+        </center>
+          <ul class="pager">
+                <li class="previous"><a href="introduccionkanji.php">Anterior</a></li>
+                <li class="next"><a href="numeros.php">Siguiente</a></li>
+            </ul>  
+        </div>        
+    </div>
+    <?php
+    include "sidenav.php";
+    ?>
+</body>
+</html>
